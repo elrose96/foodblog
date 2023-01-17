b6e6949d988c446b5e0f59d0ac015064138b63e6
elrose96 <106094763+elrose96@users.noreply.github.com>
2023-01-17 12:46:58 -0500

    Repackage and add new features to foodblog

---
+++ foodblog/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '6bdad797d13da87c5c3f4da4392e7068'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+# To avoid circular imports we need to place these after the initialization
+from foodblog import routes
---
+++ run.py
@@ -0,0 +1,4 @@
+from foodblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
