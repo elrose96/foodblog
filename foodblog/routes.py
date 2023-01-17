54048b6e752fe72b8bc71634878719339ffb56b9
elrose96 <106094763+elrose96@users.noreply.github.com>
2023-01-17 16:58:43 -0500

    Updates to Flaskblog

--- foodblog/routes.py
+++ foodblog/routes.py
@@ -1,27 +1,17 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from foodblog import app, db, bcrypt 
-from foodblog.forms import RegistrationForm, LoginForm, UpdateAccountForm 
+from foodblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm 
 from foodblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Elise Mai-Rose',
-        'title': 'Blog Post 1',
-        'content': 'First post',
-        'date': 'Janurary 9, 2023'
-    },
-    {
-        'author': 'Nissa Mai-Rose',
-        'title': 'Blog Post 2',
-        'content': 'Second post',
-        'date': 'Janurary 10, 2023'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def index():
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
 
 @app.route("/about")
@@ -62,11 +52,28 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    # Resize the profile pic 
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data 
         db.session.commit()
@@ -77,3 +84,15 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('index')) 
+    return render_template('create_post.html', title='New Post', form=form)
